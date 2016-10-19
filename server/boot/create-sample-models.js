117b66a204363732082c6fe96b9eaba3f9fc1fd1
Golan Avraham <golan_a@msn.com>
2016-10-19 20:17:33 +0300

    Configure datasources
    Set memory db to store in json file.
    Add boot script (commented) for init boot data.

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,21 @@
+// Below is an example of a boot script that LoopBack executes when an application initially starts up.
+// module.exports = function (app) {
+//     app.models.coffeeShop
+//         app.models.CoffeeShop.create([{
+//             name: 'Abraxas',
+//             city: 'Amsterdam'
+//         }, {
+//             name: 'Bel Cafe',
+//             city: 'Vancouver'
+//         }, {
+//             name: 'Three Bees Coffee House',
+//             city: 'San Mateo'
+//         }, {
+//             name: 'Caffe Artigiano',
+//             city: 'Vancouver'
+//         },], function (err, coffeeShops) {
+//             if (err) throw err;
+
+//             console.log('Models created: \n', coffeeShops);
+//         });
+// };
